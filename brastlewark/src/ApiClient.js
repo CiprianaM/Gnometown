1366f7f81c7d053986d2aed68e2eba38620d182e
CiprianaM <cipriana2001@gmail.com>
2020-04-07 10:14:27 +0200

    add fetch service

---
+++ brastlewark/src/ApiClient.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const BASE_URL = 'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json';
+const CACHE_URL ='';
+
+export default {
+  getAllGnomes: () => {
+    return fetchRequest(BASE_URL);
+  },
+  getDetails: (id) => {
+    return fetchDetails(`${CACHE_URL}/${id}`)
+  }
+};
+
+const fetchRequest = (url) => {
+  return fetch(url)
+    .then(res => res.status <= 400 ? res : Promise.reject(res))
+    .then(res => res.json())
+    .catch((err) => {
+      console.log(`${err.message} while fetching /${url}`)
+    });
+};
