ab06d9d07f092fe2c26ddd7c1e809b92b77aadc9
CiprianaM <cipriana2001@gmail.com>
2020-04-07 17:47:29 +0200

    add custom format material ui

--- brastlewark/src/App.js
+++ brastlewark/src/App.js
@@ -1,18 +1,36 @@
 import React, {useState, useEffect} from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './App.css';
 import GnomeList from "./Components/GnomeList";
 import ApiClient from "./Services/ApiClient";
+import Navbar from "./Components/Navbar";
+
+export const GnomeContext = React.createContext();
 
 function App() {
+  const myTheme = createMuiTheme({
+    typography: {
+     "fontFamily": "\"Patrick Hand\", \"Helvetica\", \"Arial\", sans-serif",
+     "fontSize": 16,
+     "fontWeightLight": 300,
+     "fontWeightRegular": 400,
+     "fontWeightMedium": 500
+    }
+  });
   const [gnomes, setGnomes] = useState([]);
   useEffect(()=> {
     ApiClient.getAllGnomes()
   .then(gnomes => setGnomes(gnomes)
   )}, [])
   return (
-    <div className="App">
-      <GnomeList gnomes={gnomes} />
-    </div>
+      <GnomeContext.Provider value={gnomes}>
+        <MuiThemeProvider theme={myTheme}>
+            <div className="App">
+              <Navbar />
+              <GnomeList gnomes={gnomes} />
+            </div>
+        </MuiThemeProvider>
+      </GnomeContext.Provider>
   );
 }
 
