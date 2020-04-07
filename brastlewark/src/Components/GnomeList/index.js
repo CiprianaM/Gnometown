e70c92d454982777cabf1ac610b63a865e1312e3
CiprianaM <cipriana2001@gmail.com>
2020-04-07 12:01:15 +0200

    pass info to GnomeTile

--- brastlewark/src/Components/GnomeList/index.js
+++ brastlewark/src/Components/GnomeList/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import GnomeTile from "../GnomeTile";
 
 export default function GnomeList ({gnomes}) {
-  console.log(gnomes)
   return (
-    <div></div>
+    <ul>
+      {gnomes.Brastlewark ?
+        gnomes.Brastlewark.slice(0, 20).map(gnome => (<GnomeTile key={gnome.id} name={gnome.name} gnomeThumbnail={gnome.thumbnail} />)) :
+        "Still loading..."
+      }
+    </ul>
   )
 }
