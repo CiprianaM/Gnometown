ad1dc5277ba99b7c58a17f916c7412500db7b0bf
CiprianaM <cipriana2001@gmail.com>
2020-04-07 16:01:16 +0200

    add gender function

--- brastlewark/src/Components/GnomeList/index.js
+++ brastlewark/src/Components/GnomeList/index.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import GnomeTile from "../GnomeTile";
 import Spinner from "../Spinner";
 import "./styles.css";
 
 export default function GnomeList ({gnomes}) {
+  // const [gender, setGender]=useState('');
+
+  function decideGender (gnome) {
+    const {name} = gnome;
+    const firstName = name.split(' ')[0]
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    // const gender = vowels.indexOf(firstName[firstName.length-1])
+    // console.log(firstName, gender)
+    const gender = vowels.indexOf(firstName[firstName.length-1])>=0 ? 'Gnomette' : 'Gnome';
+    return gender
+  }
+
   return (
     <div className="tile_cont">
       {gnomes.Brastlewark ?
@@ -14,6 +26,7 @@ export default function GnomeList ({gnomes}) {
           thumbnail={gnome.thumbnail}
           professions={gnome.friends.length}
           friends={gnome.friends.length}
+          gender={decideGender(gnome)}
         />)) :
         <Spinner />
       }
