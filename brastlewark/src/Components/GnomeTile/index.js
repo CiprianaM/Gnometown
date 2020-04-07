760e4dc7d0732804db33c49ede3b155d792f31b6
CiprianaM <cipriana2001@gmail.com>
2020-04-07 16:01:42 +0200

    add gender in GnomeTile

--- brastlewark/src/Components/GnomeTile/index.js
+++ brastlewark/src/Components/GnomeTile/index.js
@@ -6,7 +6,7 @@ export default function GnomeTile (props) {
     <div className="gnome_container">
       <img alt="gnome-img" src={props.thumbnail} className="gnome_img"/>
       <div className="gnome_headline">
-      <p className="gnome_headline_title">{props.name}</p>
+  <p className="gnome_headline_title">{props.name}, {props.gender}</p>
       </div>
     </div>
   )
