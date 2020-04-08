a9118f29acf3d00bfbc6dea47f902b448f5a17fd
CiprianaM <cipriana2001@gmail.com>
2020-04-08 13:10:21 +0200

    add lazy load functionality

--- brastlewark/src/Components/GnomeTile/index.js
+++ brastlewark/src/Components/GnomeTile/index.js
@@ -1,13 +1,16 @@
 import React, {useContext} from "react";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import {GnomeContext} from "../../App";
 import "./styles.css";
 
 export default function GnomeTile (props) {
   const context = useContext(GnomeContext);
-  console.log(context)
   return (
     <div className="gnome_container">
-      <img alt="gnome-img" src={props.thumbnail} className="gnome_img"/>
+      <LazyLoadImage
+        alt="gnome-img"
+        src={props.thumbnail}
+        width="250px" />
       <div className="gnome_headline">
   <p className="gnome_headline_title">{props.name}, {props.gender}</p>
       </div>
