0bd2d697d92507f4072e9c20c30a3cfd17db197c
CiprianaM <cipriana2001@gmail.com>
2020-04-14 15:53:03 +0200

    implement cache in front end ApiClient

--- brastlewark/src/Services/ApiClient.js
+++ brastlewark/src/Services/ApiClient.js
@@ -1,10 +1,33 @@
+// import { getAllGnomes } from "../../../server/controllers.js/cache";
 
 const BASE_URL = 'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json';
+const HOME_URL = 'http://localhost:3005';
 
 
 export default {
-  getAllGnomes: () => {
-    return fetchRequest(BASE_URL)
+  getInitialGnomes: async () => {
+    const data = await fetch('http://localhost:3005/all')
+      .then(res => res.json());
+      const dataArr = Object.values(data);
+      if (dataArr.length) {
+      return dataArr;
+    } else {
+      return fetchRequest(BASE_URL);
+    }
+  },
+  getApiGnomes: async () => {
+    return fetchRequest(BASE_URL);
+  },
+  cacheGnomes:async () => {
+    const data = await fetchRequest(BASE_URL);
+      const info =data.slice(0, 200).map(element => ({key: element.id, val: element}))
+       fetch(HOME_URL+'/all', {
+        method: 'POST',
+        body: JSON.stringify(info),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
   }
 };
 
@@ -14,12 +37,8 @@ const fetchRequest = (url) => {
       .then(res => res.json())
       .then(res=>{
         const {Brastlewark: response} = res;
-        const partialResp = response.slice(0, 100)
-        localStorage.setItem('myData', JSON.stringify(partialResp));
-        // console.log('this is res: ', res)
-        const loc = localStorage.getItem('myData');
-        // console.log('this is localstorage: ', JSON.parse(loc));
-        return res;
+        console.log(response)
+        return response;
       })
       .catch((err) => {
         console.log(`${err.message} while fetching /${url}`)
