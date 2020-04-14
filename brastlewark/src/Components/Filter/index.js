6ccffca0ac39f0985309f9dbce067dd09cd993ff
CiprianaM <cipriana2001@gmail.com>
2020-04-14 18:57:55 +0200

    update  filter component to text search

--- brastlewark/src/Components/Filter/index.js
+++ brastlewark/src/Components/Filter/index.js
@@ -1,45 +1,14 @@
-import React, {useContext} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Chip from '@material-ui/core/Chip';
-import Paper from '@material-ui/core/Paper';
+import React, {useContext} from "react";
 import {GnomeContext} from "../../App";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    padding: theme.spacing(0.5),
-    backgroundColor: "#fff",
-    borderRadius: 20
-  },
-  chip: {
-    margin: theme.spacing(0.5),
-    backgroundColor: "#282a36",
-    color: "#fff"
-  },
-}));
-
-export default function ChipsArray() {
-
-  const classes = useStyles();
-  const {chipData, handleClick} =useContext(GnomeContext);
+export default function Filter () {
+  const {handleChange} = useContext(GnomeContext);
   return (
-    <>
-    <Paper className={classes.root}>
-      {chipData.map((data) => {
-        return (
-          <Chip
-            size="small"
-            key={data.key}
-            clickable
-            label={data.label}
-            onClick={handleClick(data)}
-            className={classes.chip}
-          />
-        );
-      })}
-    </Paper>
-    </>
-  );
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" autoComplete="off" onChange={handleChange}/>
+      </label>
+</form>
+  )
 }
