2b65c82128259b69bf252357161aaa08eb7b419b
CiprianaM <cipriana2001@gmail.com>
2020-04-14 15:52:05 +0200

    add alternate data source GnomeList

--- brastlewark/src/Components/GnomeList/index.js
+++ brastlewark/src/Components/GnomeList/index.js
@@ -5,24 +5,36 @@ import {GnomeContext} from "../../App";
 import Spinner from "../Spinner";
 import "./styles.css";
 
-export default function GnomeList ({gnomes}) {
-  const {filteredGnomes} = useContext(GnomeContext);
-  console.log(filteredGnomes, 'ff')
+export default function GnomeList () {
+  const {gnomes, extendedGnomes} = useContext(GnomeContext);
   function decideGender (gnome) {
     const {name} = gnome;
     const firstName = name.split(' ')[0]
     const vowels = ['a', 'e', 'i', 'o', 'u'];
     const gender = vowels.indexOf(firstName[firstName.length-1])>=0 ? 'Gnomette' : 'Gnome';
-    return gender
+    return gender;
   }
 
   return (
     <div className="tile_cont">
-      {filteredGnomes.length ?
-        filteredGnomes.slice(0, 50).map(gnome => {
+      {extendedGnomes.length ?
+        extendedGnomes.map(gnome => {
           return (
         <GnomeTile
           key={gnome.id}
+          id={gnome.id}
+          name={gnome.name}
+          thumbnail={gnome.thumbnail}
+          professions={gnome.friends.length}
+          friends={gnome.friends.length}
+          gender={decideGender(gnome)}
+        />)}) :
+        gnomes.length ?
+        gnomes.map(gnome => {
+          return (
+        <GnomeTile
+          key={gnome.id}
+          id={gnome.id}
           name={gnome.name}
           thumbnail={gnome.thumbnail}
           professions={gnome.friends.length}
