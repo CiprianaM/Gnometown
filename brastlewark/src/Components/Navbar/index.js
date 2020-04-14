c44ef51ae46b26f6928d1b98540c52080204ff8e
CiprianaM <cipriana2001@gmail.com>
2020-04-14 19:00:28 +0200

    add back filter into Navbar

--- brastlewark/src/Components/Navbar/index.js
+++ brastlewark/src/Components/Navbar/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Filter from "../Filter";
 import "./styles.css";
 
 export default function Navbar () {
   return (
     <div className="navbar_container">
       <div className="filter_title">Choose your destiny! </div>
+      <Filter />
     </div>
   )
 }
